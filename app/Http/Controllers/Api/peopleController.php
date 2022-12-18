2c6f10cb6423020c244e08fa43753d526729b3ed
dawib19992 <115374337+dawib19992@users.noreply.github.com>
2022-12-18 17:58:32 +0100

    Add files via upload

---
+++ app/Http/Controllers/Api/peopleController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\StorepeopleRequest;
+use App\Http\Controllers\Controller;
+use App\Models\people;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class peopleController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json(people::all(), 200);
+    }
+
+    public function create(StorepeopleRequest $request): JsonResponse
+    {
+        $people = people::create($request->all());
+
+        return response()->json($people, 201);
+    }
+
+    public function read(people $people): JsonResponse
+    {
+        return response()->json($people, 200);
+    }
+
+    public function update(Request $request, people $people): JsonResponse
+    {
+        $people->update($request->all());
+
+        return response()->json($people, 200);
+    }
+
+    public function delete(people $people): JsonResponse
+    {
+        $people->delete();
+
+        return response()->json(null, 204);
+    }
+}
